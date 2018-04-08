8ec5d47cb450ac17485880d874c1012a43b203f8
Vivek govind ganesan <vivek122091@gmail.com>
2018-04-08 21:45:36 +0530

    Delete Comparison operators.py

--- Comparison operators.py
+++
@@ -1,7 +0,0 @@
-print(2 == 2)
-print('hello'=='bye')
-print('Bye'=='bye')#capitalization counts when using strings
-print('2' == 2) # string is not equal to the number
-print(2.0 == 2) # floating point numbers are equal to integers
-
-
